04c261d09071de8e2a92f194914279a5c932d6c4
Sravan <122646039+Sravan879@users.noreply.github.com>
2024-06-28 17:31:46 +0530

    Update index.js

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,26 +1,21 @@
-import {MdRemoveShoppingCart} from 'react-icons/md'
-import Popup from 'reactjs-popup'
-
 import CartContext from '../../context/CartContext'
 
 import Header from '../Header'
-import CartListView from '../CartListView'
-
 import EmptyCartView from '../EmptyCartView'
+import CartListView from '../CartListView'
 import CartSummary from '../CartSummary'
 
 import './index.css'
-
-const contentStyle = {borderRadius: '10px'}
 
 const Cart = () => (
   <CartContext.Consumer>
     {value => {
       const {cartList, removeAllCartItems} = value
       const showEmptyView = cartList.length === 0
-      const removeAll = () => {
+      const onClickRemoveAllBtn = () => {
         removeAllCartItems()
       }
+
       return (
         <>
           <Header />
@@ -29,48 +24,14 @@ const Cart = () => (
               <EmptyCartView />
             ) : (
               <div className="cart-content-container">
-                <div className="heading-remove-header">
-                  <h1 className="cart-heading">My Cart</h1>
-                  <div className="popup-container">
-                    <Popup
-                      modal
-                      {...{contentStyle}}
-                      trigger={
-                        <button type="button" className="remove-all-button">
-                          <div className="cartDiv">
-                            <MdRemoveShoppingCart size={20} color="#0967d2" />
-                          </div>
-                          <p className="remove-all">Remove All</p>
-                        </button>
-                      }
-                    >
-                      {close => (
-                        <div className="modal-container">
-                          <p className="modal-heading">
-                            Are you sure you want to delete all CartItems?
-                          </p>
-                          <div className="model-buttons">
-                            <button
-                              type="button"
-                              className="remove-ok"
-                              onClick={removeAll}
-                            >
-                              ok
-                            </button>
-                            <button
-                              type="button"
-                              className="remove-ok cancel"
-                              onClick={() => close()}
-                            >
-                              cancel
-                            </button>
-                          </div>
-                        </div>
-                      )}
-                    </Popup>
-                  </div>
-                </div>
-
+                <h1 className="cart-heading">My Cart</h1>
+                <button
+                  type="button"
+                  className="remove-all-btn"
+                  onClick={onClickRemoveAllBtn}
+                >
+                  Remove All
+                </button>
                 <CartListView />
                 <CartSummary />
               </div>
@@ -81,4 +42,6 @@ const Cart = () => (
     }}
   </CartContext.Consumer>
 )
+
 export default Cart
+
